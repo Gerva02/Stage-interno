192d769eec887cc008405d3bb3e814935ac2dc3f
Gerva02 <gerva02@gmail.com>
2024-03-01 18:49:06 +0100

    added the latent state variabile

--- simulation data.R
+++ simulation data.R
@@ -1,20 +1,25 @@
 library(tidyverse)
 n <- 1000
 n_change_points <- 5
-tau <- seq(from=0, to=1000, length.out= n_change_points+1)
-means <- rbeta(n_change_points,shape=10, shape2 = 50)
-probs <- rep(NA, n)
+tau <- seq(from=0, to=1000, length.out= n_change_points+1)  # the time t in wich we have a change
+means <- rbeta(n_change_points,shape=10, shape2 = 50) # the means of the binomial 
+probs <- rep(NA, n) #mean reapeated
+latent_state <- rep(NA, n)
+
 for (i in 1:n_change_points){
   probs[ (tau[i]+1) : tau[i+1] ]<- rep(means[i],tau[i+1] - tau[i])
+  latent_state[ (tau[i]+1) : tau[i+1] ]<- rep(i,tau[i+1] - tau[i])
 }
 
 noise <- rbinom(n,size=1, prob= probs )
 
 series <- tibble(
   t = 1:n ,
-  series = cumsum(noise))
+  series = cumsum(noise),
+  ls = as.factor(latent_state)
+  )
 
 series %>%
-  ggplot(aes(y=series, x= t)) + 
+  ggplot(aes(y=series, x= t, color= ls)) + 
   geom_line()
   
