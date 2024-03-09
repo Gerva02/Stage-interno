b7dfa0ddc8632208d1a41ab95405d324fc943edc
Gerva02 <gerva02@gmail.com>
2024-03-09 18:59:09 +0100

    aggiunto commenti e creato la simulazione di causalità nel caso di un random control trial

---
+++ causality_sim.R
@@ -0,0 +1,54 @@
+library(tidyverse)
+
+n <- 1000# con numeri più alti SDO = ATE
+age <- runif(n, 0 , 100)
+
+Yobs_lik <- function(min,age, tr=1 ){
+  if (tr==1){  
+    Yobs = dpois(min,age/2 ) 
+  }
+  if (tr==0){  
+    Yobs = dpois(min,age/1.4 ) 
+  }
+  Yobs
+} # la likelihood è ovviamente per trattamento e no
+
+switching <- function(anni, tr=1 ){
+  Y_obs <- tr*rpois(n,anni/2)+ (1-tr)*rpois(n,anni/1.4) #questa è esattamente la switching equation
+}
+
+possibile_ages <- seq(from=1 , to = 100, length.out= 10)
+possibile_min <- 0:90
+grid_1 <- outer(possibile_min,possibile_ages, Yobs_lik )
+grid_0 <- outer(possibile_min,possibile_ages, Yobs_lik, tr=0 ) 
+persp(possibile_min, possibile_ages,grid_1) #questo è il grafico f(y|X, D=1) (z è una probabilità)
+persp(possibile_min, possibile_ages,grid_0) #questo è il grafico f(y|X, D=1) (z è una probabilità)
+#vediamo quindi che con l'età il malditesta dura di più e per i non trattati dura di più
+
+ATE <- 50.5/2 - 50.5/1.4
+#previsione teorica che deriva da prob condizionata
+
+rct <- tibble(
+  age = sample(1:100, n , replace = T),
+  D = sample(0:1, n , replace = T),
+  headacke_length = switching(anni = age , tr= D)
+)
+rct
+# tibble con y_obs (headacke_length) 
+# abbiamo un random control trial ( no selection bias)
+
+
+rct %>%
+  ggplot(aes(x=age, y=headacke_length,color=factor(D)))+
+  geom_point(alpha = 0.8)+
+  scale_color_discrete(name = "Trattemento",
+                      labels = c("Placebo", "Medicina"))
+#più si va avanti più la lunghezza dei mal di testa aumenta come la varianza (poisson)
+
+
+SDO <- mean(rct%>% filter(D==1) %>% pull(headacke_length)) - mean(rct %>% filter(D==0) %>% pull(headacke_length))
+#Qua calcoliamo quello che il mixtape chiama SDO che essendo questo un random control trial  
+#sappiamo che SDO == ATE con n -> inf
+
+SDO
+ATE
--- simulation data.R
+++ simulation data.R
@@ -1,21 +1,21 @@
 library(tidyverse)
 n <- 1000
-n_change_points <- 5
-tau <- seq(from=0, to=1000, length.out= n_change_points+1)  # the time t in wich we have a change
-means <- rbeta(n_change_points,shape=10, shape2 = 50) # the means of the binomial 
-probs <- rep(NA, n) #mean reapeated
-latent_state <- rep(NA, n)
+n_change_points <- 5 #numero di change points
+tau <- seq(from=0, to=1000, length.out= n_change_points+1)  #tempo t in cui si campia
+means <- rbeta(n_change_points,shape=10, shape2 = 50) # le medie non ripetute delle binomiali
+probs <- rep(NA, n) #le medie ripetute 
+latent_state <- rep(NA, n) #questo rappresenta il numero dello stato latente s_t (chibbs)
 
 for (i in 1:n_change_points){
   probs[ (tau[i]+1) : tau[i+1] ]<- rep(means[i],tau[i+1] - tau[i])
   latent_state[ (tau[i]+1) : tau[i+1] ]<- rep(i,tau[i+1] - tau[i])
 }
 
-noise <- rbinom(n,size=1, prob= probs )
+noise <- rbinom(n,size=1, prob= probs) # qui vengono generati i singoli 0 e 1 
 
 series <- tibble(
   t = 1:n ,
-  series = cumsum(noise),
+  series = cumsum(noise), #qua si fa la somma cumulata dei noise
   ls = as.factor(latent_state)
   )
 
