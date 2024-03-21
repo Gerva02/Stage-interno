35679d42213fc1c0059236006522e19b70444872
Gerva02 <gerva02@gmail.com>
2024-03-21 18:04:44 +0100

    correction suggested during meeting

--- intial_birthweight_analysis.R
+++ intial_birthweight_analysis.R
@@ -20,19 +20,21 @@ birthwt%>%
   ggpairs(mapping = aes(color = birthwt$low))
 
 tr <- pull(birthwt, smoke)
-Y <- ifelse(pull(birthwt, low)[i] ==1 , )
+Y <- pull(birthwt, low)
 
 fit <- glm(data=birthwt, smoke ~ age + race,family = binomial(link="logit"))
-grups<-cut(fit$fitted.values,5)
+groups<-cut(fit$fitted.values,5)
 
-rr1 <-  Match(Y = Y , X = fit$fitted.values, Tr = as.vector(tr) ) #tr va convertito
-summary(rr1)#p value troppo grande se non dividiamo nei 5 gruppi 
 
 #e quindi Baldi divide in 5 gruppi rispetto il propensity score e testa il fisher sharp null 
 #usando il test chi quadro di Cochran
 
 #questo è sicuramente sbagliato perché non stimamo il test chi quadro 
-rr2 <-  Match(Y = Y , X = as.numeric(buckets), Tr = as.vector(tr) ) 
-summary(rr2) # qua stima il t value non 
+rr1 <-  Match(Y = Y , X = as.numeric(buckets), Tr = as.vector(tr) ) 
+summary(rr1) # qua stima il t value non 
 #non so quanto sia valido come risultato però vediamo un ate >0 (dunque c'è un effetto causale tra il fumare e il basso peso del feto)
 
+# 
+# rr2 <-  Match(Y = Y , X = fit$fitted.values, Tr = as.vector(tr) ) #tr va convertito
+# summary(rr2)#p value troppo grande se non dividiamo nei 5 gruppi 
+# 
